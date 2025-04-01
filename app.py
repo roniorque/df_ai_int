e16fcf0e6da2000c87f5717af5e66d26c2f34bab
Ronio Jerico Roque <roniojericho144@gmail.com>
2025-04-01 08:09:57 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+# Create an empty container
+placeholder = st.empty()
+load_dotenv()
+
+actual_email = os.getenv('email')
+actual_password = os.getenv('password')
+
+# Insert a form in the container
+with placeholder.form("login"):
+    st.markdown("#### Digital Footprint AI Team")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submit = st.form_submit_button("Login")
+
+if submit and email == actual_email and password == actual_password:
+    st.switch_page("pages/home.py")
+    st.success("Login successful")
+elif submit and email != actual_email and password != actual_password:
+    st.error("Login failed")
+else:
+    pass
