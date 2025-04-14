73d10db8bbf3f9b98159147d6ac803597ae5cbd0
Ronio Jerico Roque <roniojericho144@gmail.com>
2025-04-14 08:44:11 +0800

    Refactor client summary processing to initialize session state for client name and website, and update telemetry collection logic

--- classes/client_summary.py
+++ classes/client_summary.py
@@ -22,25 +22,45 @@ class CientSummary:
         load_dotenv()
         if 'client_summary' not in st.session_state:
             st.session_state['client_summary'] = ''
+        if 'client_name' not in st.session_state:
+            st.session_state['client_name'] = ''
+        if 'client_webiste' not in st.session_state:
+            st.session_state['client_webiste'] = ''
     
     def process (self):
             with st.spinner('Seo Analyst...', show_time=True):
                         st.write('')
                         client_summary = ""
-                        # INITIALIZING SESSIONS
-                        client_summary += f"Client Summary: {self.client_summary}\n"
+                        client_name = ""
+                        client_website = ""
+                        # 
+                        client_summary = f"Client Summary: {self.client_summary}\n"
+                        client_name = f"Client Name: {self.name}\n"
+                        client_website = f"Client Website: {self.website}\n"
 
                         debug_client_summary = {'data_field' : 'Client Summary', 'result': client_summary}
-                        
+                        debug_client_name = {'data_field' : 'Client Name', 'result': client_name}
+                        debug_client_website = {'data_field' : 'Client Website', 'result': client_website}
+
                         if self.client_summary:
                             st.session_state['client_summary'] = 'uploaded'
                             collect_telemetry(debug_client_summary)
+                        if self.name:
+                            st.session_state['client_name'] = 'uploaded'
+                            collect_telemetry(debug_client_website)
+                        if self.website:
+                            st.session_state['client_webiste'] = 'uploaded'
+                            collect_telemetry(debug_client_name)
              
     def row1(self):
             self.client_summary = st.text_area("Client Summary:", help="Name of business, nature of business, location, products/services")
             session = st.session_state.analyze
-            if self.client_summary and session == 'clicked':
+            self.name = st.text_input("Client Name:")
+            self.website = st.text_input("Client Website:")
+
+            if (self.client_summary or self.name or self.website) and session == 'clicked':
                 self.process()
+
 
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
