979c872b861a99ad2cd7ce104159fe3cf5152ba8
Ronio Jerico Roque <roniojericho144@gmail.com>
2025-04-21 12:34:32 +0800

    Handle empty payload in SocialMedia by appending "N/A" when no data is fetched

--- classes/response_social_media.py
+++ classes/response_social_media.py
@@ -120,6 +120,7 @@ class SocialMedia:
     
             self.payload += self.fetch_data("Tiktok")
             if self.payload == "":
+                 
                  self.payload += "N/A"
             summary = self.fetch_data("Client Summary")
             self.payload = summary + self.payload
