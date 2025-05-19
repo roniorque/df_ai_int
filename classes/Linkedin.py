db854665c3c7f5b5988abf2cac2767d9bc4a03d9
Ronio Jerico Roque <roniojericho144@gmail.com>
2025-05-19 09:21:16 +0800

    Add competitor name handling in Linkedin, Tiktok, and YouTube classes to enhance data processing

--- classes/Linkedin.py
+++ classes/Linkedin.py
@@ -176,6 +176,11 @@ class Linkedin:
                             
                             #end_time = time.time()
                             #time_lapsed = end_time - start_time
+                            
+                            self.competitor_name = st.session_state.competitor_name
+                            self.is_competitor = st.session_state.is_competitor
+                            combined_text = self.competitor_name + combined_text if self.is_competitor == True else combined_text
+                            
                             debug_info = {'data_field' : 'Linkedin', 'result': combined_text}
                             '''
                             debug_info = {
