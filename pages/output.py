4f7b785d2211bd3bd5b994da432efa8c58c1238b
Ronio Jerico Roque <roniojericho144@gmail.com>
2025-05-22 09:34:25 +0800

    Refactor data retrieval in write_client_footprint to access nested table structures for web, SEO, and social media responses

--- pages/output.py
+++ pages/output.py
@@ -12,18 +12,21 @@ def write_client_footprint():
 
     try:
         web = get_analyst_response("Website and Tools Analyst")
+        web = web["table"]
         result_web = {item["category"]: item["current_footprint"] for item in web}
     except TypeError:
         result_web = None
 
     try:
         seo = get_analyst_response("SEO Analyst")
+        seo = seo["table"][0]["seo"]
         seo = {item["category"]: item["current_footprint"] for item in seo}
     except TypeError:
         seo = None
 
     try:
         socmed = get_analyst_response("Social Media Analyst")
+        socmed = socmed["table"]
         socmed = {item["category"]: item["current_footprint"] for item in socmed}
     except TypeError:
         socmed = None
